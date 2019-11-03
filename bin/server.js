89a0616c941eedba4bcb7a7f183641b336b06d9c
DevDanielRabelo <danielrabelo2000@gmail.com>
2019-11-03 17:15:50 -0300

    Subindo API

---
+++ bin/server.js
@@ -0,0 +1,12 @@
+const app = require('../src/app');
+const http = require('http');
+// const debug = require('debug')('nodestr:server');
+
+
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
+console.log('API rodando na porta ' + port);
---
+++ src/config.js
@@ -0,0 +1,8 @@
+global.SALT_KEY = 'd0bc31eb4842977a6c10a42475d2fd85';
+global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao Teste Node.js';
+
+module.exports = {
+    connectionString: 'mongodb+srv://balta:balta@nodebd-bdwd4.azure.mongodb.net/test?retryWrites=true&w=majority',
+    sendgridkey: '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',
+    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=testedaniel;AccountKey=fAo02HBX5aTsn5/CMNU6+Q3zdWHNUF48Bb9dkH99m8GbFKlOll+P3MGQH4aoNfPr6sx8VSuv5JOYc0K2YT7rvg==;EndpointSuffix=core.windows.net'
+}
