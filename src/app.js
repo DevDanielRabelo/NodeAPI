a39e840694f73f0e77bc48dece0fa0673badd415
unknown <danielrabelo2000@gmail.com>
2020-05-20 19:43:33 -0300

    ''

--- src/app.js
+++ src/app.js
@@ -8,31 +8,31 @@ const config = require('./config');
 const app = express();
 
 //Conect ao banco
-mongoose.connect(config.connectionString);
+// mongoose.connect(config.connectionString);
 
 //Carregar os Models
-const Product = require('./models/product');
-const Customer = require('./models/customer');
-const Order = require('./models/order');
+// const Product = require('./models/product');
+// const Customer = require('./models/customer');
+// const Order = require('./models/order');
 
 //Carregar as Rotas
-const indexRoute = require('./routes/index-route');
-const productRoute = require('./routes/product-route');
-const customerRoute = require('./routes/customer-route');
-const orderRoute = require('./routes/order-route');
+// const indexRoute = require('./routes/index-route');
+// const productRoute = require('./routes/product-route');
+// const customerRoute = require('./routes/customer-route');
+// const orderRoute = require('./routes/order-route');
 
-app.use(bodyParser.json({
-    limit: '5mb'
-}));
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json({
+//     limit: '5mb'
+// }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 // Habilita o CORS
-app.use(function (req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});
+// app.use(function (req, res, next){
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     next();
+// });
 
 const nodemailer = require('nodemailer');
 
@@ -64,10 +64,10 @@ transporter.sendMail(mailOptions, function(error, info){
 
 
 
-app.use('/', indexRoute);
-app.use('/products', productRoute);
-app.use('/customers', customerRoute);
-app.use('/orders', orderRoute);
+// app.use('/', indexRoute);
+// app.use('/products', productRoute);
+// app.use('/customers', customerRoute);
+// app.use('/orders', orderRoute);
 
 module.exports = app;
 
